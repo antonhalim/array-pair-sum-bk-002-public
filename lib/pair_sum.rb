a238b88ad862023cec95d4106e42913478f0b545
axton21 <axton_n@yahoo.com>
2015-05-10 14:26:24 -0400

    done

--- lib/pair_sum.rb
+++ lib/pair_sum.rb
@@ -1,1 +1,14 @@
-# your code goes here
+class Array
+	def pair_sum(num)
+		result_array = Array.new
+		self.each do |first|
+			self.each do |second|
+				if first + second == num
+					result_array << [first, second].sort if !result_array.include?([first, second].sort)
+				end
+			end
+		end
+		result_array.sort
+	end
+
+end
